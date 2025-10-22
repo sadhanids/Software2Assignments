75981f0813be994f18899f8660bdc9cec8c0e689
sadhanids <sadhanids@gmail.com>
2025-10-22 22:27:03 +0300

    I have completed task 3

--- Module9/classes/car.py
+++ Module9/classes/car.py
@@ -5,4 +5,13 @@ class Car:
         self.current_speed = 0
         self. travelled_distance = 0
 
+    def accelerate(self, value):
+        self.new_speed = self.current_speed + value
+        if self.new_speed > self.max_speed:
+            self.current_speed = self.max_speed
+        elif self.new_speed < 0:
+            self.current_speed = 0
+        else:
+            self.current_speed = self.new_speed
+
 
--- Module9/task2.py
+++ Module9/task2.py
@@ -1,3 +1,17 @@
-print(f"""Registration Number: {registration_number} 
-Maximum Speed: {max_speed} Current Speed: {current_speed} 
-Travelled Distance: {travelled_distance} """)
+from classes.car import Car
+
+new_car = Car("ABC-123", 142 )
+
+print(f"Initial Speed: {new_car.current_speed} km/h")
+
+new_car.accelerate(30)
+print(f"Speed Increased by 30 km/h, Current Speed: {new_car.current_speed} km/h")
+
+new_car.accelerate(70)
+print(f"Speed Increased by 70 km/h, Current Speed: {new_car.current_speed} km/h")
+
+new_car.accelerate(50)
+print(f"Speed Increased by 50 km/h, {new_car.current_speed} km/h")
+
+new_car.accelerate(-200)
+print(f"Speed Decreased by 200 km/h, {new_car.current_speed} km/h")
