9ec43203557ea68fcc68b70c2fc7c56f0bdd8c7d
sadhanids <sadhanids@gmail.com>
2025-10-24 23:57:58 +0300

    I have completed task 1 of Module 10.

---
+++ Module10/task1.py
@@ -0,0 +1,11 @@
+from classes.elevator import Elevator
+
+h = Elevator(1,10)
+
+h.go_to_floor(5)
+print("-----------------------------")
+h.go_to_floor(8)
+print("-----------------------------")
+h.go_to_floor(5)
+print("-----------------------------")
+h.go_to_floor(12)
