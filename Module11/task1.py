4e58761a101502c3da0157f5344613a8a47be766
sadhanids <sadhanids@gmail.com>
2025-11-03 02:06:15 +0300

    I have completed task 1 of Module 11.

---
+++ Module11/task1.py
@@ -0,0 +1,10 @@
+from classes.library import Book, Magazine
+
+magazine1 = Magazine(name="Donald Duck", chief_editor="Aki Hyyppä")
+
+book1 = Book(name="Compartment No. 6", author="Rosa Liksom", page_count=192)
+
+magazine1.print_information()
+book1.print_information()
+
+
